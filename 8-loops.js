d7860c721fc9936753dcad50ac393faee19b045a
cassy <128146303+cassyat@users.noreply.github.com>
2023-06-29 11:17:05 -0300

    Add files via upload

--- 8-loops.js
+++ 8-loops.js
@@ -5,10 +5,10 @@ const listaDeDestinos = new Array (
     `Rio De Janeiro`,   
 );
 
-const idadeComprador = 20; 
-const estaAcompanhada = true;
+const idadeComprador = 18; 
+const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = "Rio de Janeiro";
+const destino = "Curitiba";
 
 console.log("\n Destinos possíveis");
 console.log(listaDeDestinos);
@@ -16,11 +16,27 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
 while(contador<3){
     if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe");
-    }else{
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
-    contador +=1;
+
+    contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem");
+}else{
+    console.log("Desculpa tivemos um erro!");
+}
+
+for(let i = 0; i < 3; i ++){
+    if(listaDeDestinos[i] == destino){
+        destinoExiste = true;
+    }
+
 }
